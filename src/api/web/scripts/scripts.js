1552848f94245f34061c3d8ecfa74cc2e251be71
DevEminent <22-06230@g.batstate-u.edu.ph>
2024-03-07 15:48:24 +0800

    Add hover feature on dashboard and made the sidebar collapse

--- src/api/web/scripts/scripts.js
+++ src/api/web/scripts/scripts.js
@@ -1,6 +1,12 @@
 const container = document.getElementById('container');
+const sidebar = document.querySelector('.sidebar');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const menuBtn = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle("active");
+})
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
